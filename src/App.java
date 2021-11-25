80676d7726323603ec45c433fb845192ad3d0a1d
Sam D <88640808+Samuelsntr@users.noreply.github.com>
2021-11-25 09:35:35 +0800

    ganti string

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Good Moring!");
+        System.out.println("Good Morning!");
     }
 }
